4f409f3571c0907e688d9b2a781d7223c1665080
favirguezc <favirguezc@gmail.com>
2015-02-14 09:36:22 -0500

    Organizados packages produccion

---
+++ Escritorio/src/vista/produccion/cargarRegistros/LectorDeRegistrosHumedadDelSuelo.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista.produccion.cargarRegistros;
+
+import controlador.produccion.variablesClimaticas.HumedadDelSueloControlador;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Date;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import vista.produccion.cargarRegistros.util.CellDataExtractor;
+
+/**
+ *
+ * @author fredy
+ */
+public class LectorDeRegistrosHumedadDelSuelo {
+
+    public static int leer(File archivo) {
+        try {
+            FileInputStream file = new FileInputStream(archivo);
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            rowIterator.next();
+            rowIterator.next();
+            HumedadDelSueloControlador controlador = new HumedadDelSueloControlador();
+            int guardados = 0;
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+
+                Date fecha = CellDataExtractor.leerFecha(row.getCell(0));
+                double celda15 = CellDataExtractor.leerNumero(row.getCell(1));
+                double celda30 = CellDataExtractor.leerNumero(row.getCell(2));
+                Date hora = CellDataExtractor.leerHora(row.getCell(3));
+                if (fecha != null && hora != null) {
+                    try {
+                        controlador.guardar(controlador.nuevo(fecha, (float) celda15, (float) celda30, hora));
+                        guardados++;
+                    } catch (Exception e) {
+                    }
+                } else {
+                    System.out.println("error: " + fecha + "\t" + celda15 + "\t" + celda30 + "\t" + hora);
+                }
+            }
+            file.close();
+            return guardados;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
---
+++ Escritorio/src/vista/produccion/graficas/GeneradorDeGraficas.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista.produccion.graficas;
+
+import java.awt.Color;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.Plot;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.PieDataset;
+
+/**
+ *
+ * @author fredy
+ */
+public class GeneradorDeGraficas {
+
+    static final Color[] colores = {
+        Color.BLUE,
+        Color.GRAY,
+        Color.GREEN,
+        Color.ORANGE,
+        Color.MAGENTA,
+        Color.RED,
+        Color.CYAN};
+
+    public static JFreeChart crearGraficaDeBarras(CategoryDataset datos, String titulo, String dominio, String rango, int series) {
+        CategoryPlot plot = new CategoryPlot();
+        plot.setDomainAxis(new CategoryAxis(dominio));
+        plot.setRangeAxis(new NumberAxis(rango));
+        plot.setBackgroundPaint(Color.WHITE);
+        BarRenderer renderer = new BarRenderer();
+        renderer.setDrawBarOutline(false);
+        for (int i = 0; i < series; i++) {
+            renderer.setSeriesPaint(i, colores[i % colores.length]);
+        }
+        plot.setDataset(datos);
+        plot.setRenderer(renderer);
+        plot.getDomainAxis().setCategoryLabelPositions(CategoryLabelPositions.UP_45);
+        JFreeChart chart;
+        chart = new JFreeChart(plot);
+        chart.setTitle(titulo);
+        return chart;
+    }
+    
+    public static JFreeChart crearGraficoTorta(PieDataset datos, String titulo){
+        JFreeChart createPieChart = ChartFactory.createPieChart(titulo, datos, true, true, true);
+        Plot plot = createPieChart.getPlot();
+        plot.setBackgroundPaint(Color.WHITE);
+        return createPieChart;
+    }
+}
